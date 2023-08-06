5275eb41fed77e4da84fcfe3ef48ffe2502bce4f
Pavel Guzenfeld <pavelguzenfeld@gmail.com>
2023-08-06 23:22:25 +0200

    removed logfile

--- files/run_mavproxy.sh
+++ files/run_mavproxy.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-LOG_NAME=$(date "+%Y%m%d_%H%M%S_mavproxy")
-LOG_FILE_NAME="${LOG_NAME}.log"
-LOG_DIR="/var/log/mavproxy/"
+# LOG_NAME=$(date "+%Y%m%d_%H%M%S_mavproxy")
+# LOG_FILE_NAME="${LOG_NAME}.log"
+# LOG_DIR="/var/log/mavproxy/"
 
-cd ${LOG_DIR}
-mkdir ${LOG_NAME}
-cd ${LOG_NAME}
+# cd ${LOG_DIR}
+# mkdir ${LOG_NAME}
+# cd ${LOG_NAME}
 
-mavproxy.py --logfile=${LOG_FILE_NAME} ${@}
+# mavproxy.py --logfile=${LOG_FILE_NAME} ${@}
+mavproxy.py ${@}
